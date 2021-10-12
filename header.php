763b739b3c2707f2c7c545b7f680d036c96e3b83
Adinda Webdesignhq <a.knevel@webdesignhq.nl>
2021-10-12 14:55:11 +0200

    Changed name of project

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
  * @package WordPress
- * @subpackage AdindaMedia
+ * @subpackage Webdesignhq
  * @since 1.0.0
  */
 ?><!doctype html>
@@ -15,8 +15,6 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<link href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.css" rel="stylesheet">
-	<link href="<?php echo get_template_directory_uri(); ?>/css/style-vincent.css" rel="stylesheet">
-	<link href="<?php echo get_template_directory_uri(); ?>/css/style-adinda.css" rel="stylesheet">
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="profile" href="https://gmpg.org/xfn/11" />
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen,projection" />
